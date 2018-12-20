a6055bac91f31400849d91bfd14810c862ba5225
D0641860 <zs1452ax36f169@gmail.com>
2018-12-20 16:36:06 +0800

    web

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var jqupload = require('jquery-file-upload-middleware');
+var formidable = require('formidable');
+
 const port = 3000;
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -16,21 +18,20 @@ app.get("/", function (req, res) {
 app.get("/donate.html", function (req, res) {
     res.sendFile(__dirname + "/" + "donate.html");
 });
-
-app.post("/donate_post", urlencodedParser, function (req, res) {
-    const firstName = req.body.name;
-    const YN = req.body.checkname;
-    const phone = req.body.phone;
-    const email = req.body.email;
-    const resource = req.body.resource;
-    const pro = req.body.profession;
-    const source=req.body.source;
-    const limit=req.body.limit;
-    const Candate1=req.body.Candate1;
-    const Candate2=req.body.Candate2;
-    res.send(`
-      Hello, ${firstName} ${phone} ${YN} ${email} ${resource}  ${pro} ${source} ${limit} ${Candate1} ${Candate2}
-    `);
-    
+app.get("/test.html", function (req, res) {
+    res.sendFile(__dirname + "/" + "test.html");
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post("/donate_post", urlencodedParser, function (req, res) {
+    //console.log(req.body);
+     res.send("<html> 感謝您的填寫 請耐心等待 我們會盡快將您的資料審核並將結果寄信給您"+
+     "<script> setTimeout(window.location.href='http://localhost:3000/test.html',5000);</script></html>");
+    
+    
+    
+    // res.send("<html> 感謝您的填寫 請耐心等待 我們會盡快將您的資料審核並將結果寄信給您"+
+    // "<script> setTimeout('history.back()',5000);</script></html>");
+    // // "document.getElementById(‘show’).innerHTML = “” t “秒後跳轉到php程式設計師教程網”;"+ 
+    // "t–-; }
+    
+}); 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));	
